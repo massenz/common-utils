882884371ee2dd093935f75d8da7f4cc74c794ba
Marco Massenzio <marco@alertavert.com>
2021-01-03 00:00:18 -0800

    Implemented the build_protobuf() function
    Generalized the PB build steps into their own function.
    Needs to be tested across other projects to figure out
    corner cases where it doesn't work as intended.

--- commons.cmake
+++ commons.cmake
@@ -62,3 +62,39 @@ function(config_install INSTALL_DIR INSTALL_RESULT)
   endif()
 
 endfunction(config_install)
+
+
+# Builds the .proto Protocol Buffers defined in PROTO_SOURCES
+# The generated Sources and Headers will be listed in the respective
+# output variables (the individual files, NOT the folders).
+#
+# The generated files are in the CMAKE_BINARY_DIR folder, add it to the
+# include_directories() command (the GENERATED_SOURCES should be added to
+# the executable or library target, as appropriate (e.g., in add_executable()).
+#
+function(build_protobuf PROTO_SOURCES GENERATED_SOURCES GENERATED_HEADERS)
+    # Building Protocol Buffers.
+    # See: https://cmake.org/cmake/help/latest/module/FindProtobuf.html
+    #
+    find_package(Protobuf REQUIRED)
+    if(${PROTOBUF_FOUND})
+        message(STATUS "Protocol Buffer library version: ${Protobuf_VERSION}")
+
+        # Change to STATUS to debug PB compile issues.
+        message(TRACE "protoc: ${Protobuf_PROTOC_EXECUTABLE}")
+        message(TRACE "Protobuf Headers folder: ${Protobuf_INCLUDE_DIR}")
+        message(TRACE "protoc lib: ${Protobuf_PROTOC_LIBRARY}")
+        message(TRACE "protobuf lib: ${Protobuf_LIBRARY}")
+
+        # Needed to import .proto files from the PB distribution (e.g., any.proto)
+        set(Protobuf_IMPORT_DIRS ${Protobuf_INCLUDE_DIR})
+
+        PROTOBUF_GENERATE_CPP(GEN_SRCS GEN_HDRS ${${PROTO_SOURCES}})
+        message("Protocol Buffer generated files will be in:\n"
+                "\tHeaders: ${GEN_HDRS}\n\tSources: ${GEN_SRCS}")
+        set(${GENERATED_HEADERS} ${GEN_HDRS} PARENT_SCOPE)
+        set(${GENERATED_SOURCES} ${GEN_SRCS} PARENT_SCOPE)
+    else()
+        message(FATAL_ERROR "Could not find Google Protocol Buffers libraries")
+    endif()
+endfunction()
