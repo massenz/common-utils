0c67366a15805f3eb08358bdbdaddca4d407c804
Marco Massenzio <marco@alertavert.com>
2023-12-31 00:57:53 -0800

    Fixed copyright line

--- parse-args/parse_args.py
+++ parse-args/parse_args.py
@@ -1,15 +1,4 @@
-
 #  Copyright (c) 2020-2023 AlertAvert.com.  All rights reserved.
-#
-#  Licensed under the Apache License, Version 2.0
-#  http://www.apache.org/licenses/LICENSE-2.0
-#
-#  Author: Marco Massenzio (marco@alertavert.com)
-#
-#  Licensed under the Apache License, Version 2.0
-#  http://www.apache.org/licenses/LICENSE-2.0
-#
-#  Author: Marco Massenzio (marco@alertavert.com)
 #
 #  Licensed under the Apache License, Version 2.0
 #  http://www.apache.org/licenses/LICENSE-2.0
